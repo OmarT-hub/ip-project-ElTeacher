24f1a8875fdeec9da24873aebacd1aebfc7e9d7f
Omar <omaaremails@gmail.com>
2026-04-29 20:45:06 +0300

    Create JS File , add new function when scrollY >=600 button display

---
+++ main.js
@@ -0,0 +1,16 @@
+let btn = document.querySelector(".back-to-top");
+
+window.onscroll = function () {
+    if (window.scrollY >= 600) {
+        btn.style.display = "flex";
+    } else {
+        btn.style.display = "none";
+    }
+};
+
+btn.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+};
